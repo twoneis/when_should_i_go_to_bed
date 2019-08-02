42f4bdb61e33bb498c573cb2a2400b73886f18df
FN2187fo <48321566+FN2187fo@users.noreply.github.com>
2019-08-02 20:31:35 +0200

    Moved sources to settings

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:when_should_i_go_to_bed/appbar.dart';
 
@@ -36,6 +39,77 @@ class Settings extends StatelessWidget
               DynamicTheme.of(context).setBrightness(Theme.of(context).brightness == Brightness.dark? Brightness.light: Brightness.dark);
             },
           ),
+          Divider(),
+          FlatButton
+          (
+            child: Text("View Sources"),
+            onPressed: () 
+            {
+              showDialog
+              (
+                context: context,
+                child: ListView
+                (
+                  padding: const EdgeInsets.all(20.0),
+                  children: <Widget>
+                  [
+                    Card
+                    (
+                      child: Column
+                      (
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>
+                        [
+                          ButtonTheme.bar
+                          (
+                            child: ButtonBar
+                            (
+                              children: <Widget>
+                              [
+                                CloseButton()
+                              ],
+                            ),
+                          ),
+                          ListTile
+                          (
+                            title: Linkify
+                            (
+                              text: "How much sleep do you need at which age? https://www.sleepfoundation.org/excessive-sleepiness/support/how-much-sleep-do-we-really-need",
+                              onOpen: (url) async
+                              {
+                                if (await canLaunch(url))
+                                {
+                                  await launch(url);
+                                }
+                              },
+                            ),
+                          ),
+                          ListTile
+                          (
+                            title:Linkify
+                            (
+                              text: "Do Women need more sleep than men? https://www.sleepfoundation.org/articles/do-women-need-more-sleep-men",
+                              onOpen: (url) async
+                              {
+                                if (await canLaunch(url))
+                                {
+                                  await launch(url);
+                                }
+                              },
+                            )
+                          ),
+                          ListTile
+                          (
+                            title: Text("All datas are only based on my resarch and are average value. I'm just a normal guy trying to help others to sleep enough. If you need exact datas on how long you need to sleep, you may consider asking a doctor. Again, this data will not apply to anyone and are just average values."),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              );
+            },
+          )
         ],
       )
     );
